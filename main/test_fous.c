ec3a5ffdff6a95f6b6675ae5d213e5de58954654
Richard <richard.fan2000@hotmail.com>
2023-12-17 22:34:34 -0500

    three brightness state toggled with switch

--- main/test_fous.c
+++ main/test_fous.c
@@ -2,11 +2,14 @@
     GPIO 4 = LED
     GPIO 23 = Button
     GPIO 15 = BUTTOn
+    GPIO 22 = Toggle switch
 */
 #include <stdio.h>
 #include "driver/ledc.h"
 #include "esp_err.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -15,9 +18,26 @@
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
 #define LEDC_OUTPUT_IO          (4) // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
-#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY               (8192) // Set duty to 50%. (2 ** 13) * 50% = 4096
-#define LEDC_FREQUENCY          (1000) // Frequency in Hertz. Set frequency at 4 kHz
+#define LEDC_DUTY_RES           LEDC_TIMER_8_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (255) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_DUTY_15            (32)
+#define LEDC_DUTY_35            (128)
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+#define BUTTON1                 15
+#define BUTTON2                 23
+#define TOGGLE                  22
+
+typedef struct Data_t {
+    uint8_t current_state;
+    bool momentary_state;
+} generic_data_t;
+
+static generic_data_t TEST_DATA = {0, false};
+
+static SemaphoreHandle_t bin_sem;
+
+void listener(void *xStruct);
+void momentary_listener(void *xStruct);
 
 /* Warning:
  * For ESP32, ESP32S2, ESP32S3, ESP32C3, ESP32C2, ESP32C6, ESP32H2, ESP32P4 targets,
@@ -50,22 +70,113 @@ static void example_ledc_init(void)
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
+static void set_gpio()
+{
+    gpio_reset_pin(BUTTON1);
+    gpio_reset_pin(BUTTON2);
+    gpio_reset_pin(TOGGLE);
+
+    gpio_set_direction(BUTTON1, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON2, GPIO_MODE_INPUT);
+    gpio_set_direction(TOGGLE, GPIO_MODE_INPUT);
+    gpio_pullup_dis(BUTTON1);
+    gpio_pulldown_en(BUTTON1);
+    gpio_pullup_dis(TOGGLE);
+    gpio_pulldown_en(TOGGLE);
+    gpio_pullup_dis(BUTTON2);
+    gpio_pulldown_dis(BUTTON2);
+
+}
+
+
 void app_main(void)
 {
-    // while(1){
-    //     gpio_reset_pin(4);
-    //     gpio_set_direction(4, GPIO_MODE_OUTPUT);
-    //     gpio_set_level(4, 1);
-    //     vTaskDelay(100/portTICK_PERIOD_MS);
-    //     gpio_set_level(4,0)
-    //     vTaskDelat(100/portTICK_PERIOD_MS);
-    // }
-    
-    // Set the LEDC peripheral configuration
+    // Configure GPIO pins
+    set_gpio();
     example_ledc_init();
-    // Set duty to 50%
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
-    // Update duty to apply the new value
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    
+
+    xTaskCreate(listener, "Listener", 4000, (void *)&TEST_DATA, 1, NULL);
+    //xTaskCreate(momentary_listener, "ListenerMomentary", 3000, (void *)&TEST_DATA, 2, NULL);
+
+    // while (1)
+    // {
+    //     uint8_t toggle_switch_state = gpio_get_level(TOGGLE);
+    //     if (toggle_switch_state == 1){
+    //         char* test = (char *)malloc(sizeof(uint8_t));
+    //         itoa(toggle_switch_state, test, 10);
+    //         printf("%s\n", test);
+    //         free(test);
+    //     }
+    //     vTaskDelay(100/portTICK_PERIOD_MS);
+
+    // }
+    // uint8_t brightness_state = 0;
+    // // Current board state ranges from 0 to 7
+    // uint8_t current_state = 0;
+    // // On startup updated duty cycle to 100%
+    // ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+}
+
+// Add the control for the toggle switch to momentary;
+
+void listener(void * xStruct) {
+    generic_data_t *data = (generic_data_t *)xStruct;
+    while(1){
+        uint8_t brightness_button_state = gpio_get_level(BUTTON1);
+        uint8_t toggle_switch_state = gpio_get_level(TOGGLE);
+        vTaskDelay(100/portTICK_PERIOD_MS);
+        if (toggle_switch_state == 1)
+        {
+            data->momentary_state = true;
+        }
+        if (brightness_button_state == 1) {
+            printf("%i", data->momentary_state);
+            switch (data->current_state % 3) {
+                case 0:
+                    // Set duty to 100%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                case 1:
+                    // Set duty to 35%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_35));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                case 2:
+                    // Set duty to 15%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_15));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                default:
+                    break;
+            }
+            ++data->current_state;
+            char* test = (char *)malloc(sizeof(uint8_t));
+            itoa(brightness_button_state, test, 10);
+            printf("%s\n", test);
+            free(test);
+        }
+    }
+}
+
+void momentary_listener(void *xStruct)
+{
+    while(1)
+    {
+        generic_data_t *data = (generic_data_t *)xStruct;
+        uint8_t toggle_switch_state = gpio_get_level(TOGGLE);
+        vTaskDelay(50/portTICK_PERIOD_MS);
+        if (toggle_switch_state == 1) 
+        {
+            data->momentary_state = true;
+            printf("%i", data->momentary_state);
+        }
+        vTaskDelay(50/portTICK_PERIOD_MS);
+    }
 
 }
