35377df0bf60e4618228f25747306b4fd6b88056
Richard <richard.fan2000@hotmail.com>
2023-12-21 01:02:30 -0500

    updated logic for toggle and momentary

--- main/test_fous.c
+++ main/test_fous.c
@@ -4,6 +4,7 @@
     GPIO 15 = BUTTOn
     GPIO 22 = Toggle switch
 */
+// TODO Debounce buttons, fix logic when momentary state is changed
 #include <stdio.h>
 #include "driver/ledc.h"
 #include "esp_err.h"
@@ -23,6 +24,7 @@
 #define LEDC_DUTY               (255) // Set duty to 50%. (2 ** 13) * 50% = 4096
 #define LEDC_DUTY_15            (32)
 #define LEDC_DUTY_35            (128)
+#define LEDC_DUTY_0             0
 #define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 #define BUTTON1                 15
 #define BUTTON2                 23
@@ -31,9 +33,10 @@
 typedef struct Data_t {
     uint8_t current_state;
     bool momentary_state;
+    bool on_state;
 } generic_data_t;
 
-static generic_data_t TEST_DATA = {0, false};
+static generic_data_t TEST_DATA = {0, false, false};
 
 static SemaphoreHandle_t bin_sem;
 
@@ -43,8 +46,13 @@ void momentary_listener(void *xStruct);
 void IRAM_ATTR toggle_isr_handler(void *arg)
 {
     
-    ((generic_data_t *)arg)->momentary_state = !((generic_data_t *)arg)->momentary_state ;
+    ((generic_data_t *)arg)->momentary_state = !((generic_data_t *)arg)->momentary_state;
     //ESP_LOGI("INTERR", "INTTERRIPS");
+}
+
+void IRAM_ATTR momentary_isr_handler(void *arg)
+{
+    ((generic_data_t *)arg)->on_state = !((generic_data_t *)arg)->on_state;
 }
 
 /* Warning:
@@ -92,8 +100,7 @@ static void set_gpio()
     gpio_pullup_dis(TOGGLE);
     gpio_pulldown_en(TOGGLE);
     gpio_pullup_dis(BUTTON2);
-    gpio_pulldown_dis(BUTTON2);
-
+    gpio_pulldown_en(BUTTON2);
 }
 
 
@@ -103,10 +110,23 @@ void app_main(void)
     set_gpio();
     example_ledc_init();
 
-    // Set interrupt for both rising and falling edges
+    // Set and install interrupt for both rising and falling edges for the toggle switch 
     gpio_set_intr_type(TOGGLE, GPIO_INTR_ANYEDGE);
     gpio_install_isr_service(ESP_INR_FLAG_DEFAULT);
     gpio_isr_handler_add(TOGGLE, toggle_isr_handler, (void *)&TEST_DATA);
+    
+    // Set and install interrupt service for rising and falling edges for the momentary switch
+    gpio_set_intr_type(BUTTON2, GPIO_INTR_ANYEDGE);
+    gpio_isr_handler_add(BUTTON2, momentary_isr_handler, (void *)&TEST_DATA);
+
+    if (gpio_get_level(TOGGLE) == 0)
+    {
+        TEST_DATA.momentary_state = false;
+    }
+    else
+    {
+        TEST_DATA.momentary_state = true;
+    }
 
     xTaskCreate(listener, "Listener", 4000, (void *)&TEST_DATA, 1, NULL);
     //xTaskCreate(momentary_listener, "ListenerMomentary", 3000, (void *)&TEST_DATA, 2, NULL);
@@ -137,9 +157,10 @@ void listener(void * xStruct) {
     generic_data_t *data = (generic_data_t *)xStruct;
     while(1){
         uint8_t brightness_button_state = gpio_get_level(BUTTON1);
-        uint8_t toggle_switch_state = gpio_get_level(TOGGLE);
+        printf("%i", data->on_state);
         vTaskDelay(100/portTICK_PERIOD_MS);
-        if (brightness_button_state == 1) {
+        // If the led is currently on and momentary state is false or the momentary button is pressed and the momentary state is true
+        if (brightness_button_state == 1 && (data->on_state == 1 || data->momentary_state == 0)) {
             printf("%i", data->momentary_state);
             switch (data->current_state % 3) {
                 case 0:
@@ -169,6 +190,38 @@ void listener(void * xStruct) {
             printf("%s\n", test);
             free(test);
         }
+        else if (data->on_state == 0 && data->momentary_state == 1)
+        {
+            //Set to 0, essentially turning LED off
+            ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_0));
+            // Update duty to apply the new value
+            ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+        }
+        else if (data->on_state == 1 && data->momentary_state == 1)
+        {
+            switch (data->current_state % 3) {
+                case 0:
+                    // Set duty to 100%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                case 1:
+                    // Set duty to 35%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_35));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                case 2:
+                    // Set duty to 15%
+                    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_15));
+                    // Update duty to apply the new value
+                    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 
@@ -186,5 +239,4 @@ void momentary_listener(void *xStruct)
         }
         vTaskDelay(50/portTICK_PERIOD_MS);
     }
-
 }
